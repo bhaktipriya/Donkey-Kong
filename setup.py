d1405a2dde98f871d55db04b758707c05ea686b9
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2015-09-12 19:23:00 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from os import path
+
+
+
+
+setup(
+    name='sample',
+
+    version='1.2.0',
+
+    description='A  Pygame project',
+
+    # Author details
+    author='Bhaktipriya Shridhar',
+    author_email='bhaktipriya.r@students.iiit.ac.in',
+
+    # Choose your license
+    license='MIT',
+
+    install_requires=['pygame','pytest']
+
+    
+)
